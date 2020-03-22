f48dc314bcd2e36b249f140dfed00f4b4ac3fc81
David Garcia <david_5043@hotmail.com>
2020-03-22 10:27:53 -0600

    Modified files

--- src/com/kiyoshi/Staircase.java
+++ src/com/kiyoshi/Staircase.java
@@ -3,10 +3,10 @@ package com.kiyoshi;
 /*
 Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces.
 The last line is not preceded by any spaces.
-
 Write a program that prints a staircase of size.
 
 n=4
+
 #
 ##
 ###
@@ -15,17 +15,24 @@ n=4
 
 public class Staircase {
     public static void main(String[] args) {
-        staircase(10);
+        staircase(4);
+        System.out.println();
+        staircaseFor(4);
     }
 
     public static void staircase(Integer n){
         Integer control = 1;
-        String myC = "#";
 
         while(n > 0){
-            System.out.println(myC.repeat(control));
+            System.out.println("#".repeat(control));
             control++;
             n--;
+        }
+    }
+
+    public static void staircaseFor(Integer n){
+        for(int i = 0; i < n; i++){
+            System.out.println("#".repeat(i+1));
         }
     }
 }
